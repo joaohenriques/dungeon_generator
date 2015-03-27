00226339041ee72a58596766fcb473d529834669
joaohenriques <joaopedrohenriques@gmail.com>
2015-03-27 00:53:49 +0000

    refactored generator functions to classes

--- generators/cellular_automata.py
+++ generators/cellular_automata.py
@@ -1,138 +1,191 @@
+from __future__ import print_function
 
+import os,sys,time
 from random import random
 from enum import Enum
+import copy
 
-Tile = Enum('Tile', 'EARTH GROUND WALL FLOOR')
-
-UNKNOWN = '*'
-WALL = '#'
-EARTH = '+'
-WET_EARTH = '~'
-DRY_EARTH = '%'
-GROUND = '.'
-FLOOR = ' '
-
-def gen_square(prob):
-    if random() >= prob:
-        return UNKNOWN
-    else:
-        return GROUND
+Tile = Enum('Tile', 'EARTH WALL FLOOR')
 
 
-def gen_dungeon(height=10, width=10, prob=0.4):
-    
-    dungeon = []
-    outerwall = [WALL for _ in range(0,width)]
-    dungeon.append(outerwall)
-    for row in range(0, height-2):
-        line = [gen_square(prob) for _ in range(0,width-2)]
-        #add outer wall
-        line.append(WALL)
-        line.insert(0, WALL)
-        dungeon.append(line)
-    dungeon.append(outerwall)
-    return dungeon
 
-def evolve_dungeon(dungeon):
-    for y in range(1,len(dungeon)-1):
-        for x in range(1,len(dungeon[y])-1):
-                       
-            walls = 0
-            if dungeon[y-1][x-1] != GROUND:
-                walls += 1
-            if dungeon[y-1][x] != GROUND:
-                walls += 1
-            if dungeon[y-1][x+1] != GROUND:
-                walls += 1
+class MapTransform(object):
+    def transform(self, map):
+        raise NotImplemented
 
-            if dungeon[y][x-1] != GROUND:
-                walls += 1
-            if dungeon[y][x+1] != GROUND:
-                walls += 1
+class RandomizeMap(MapTransform):
 
-            if dungeon[y+1][x-1] != GROUND:
-                walls += 1
-            if dungeon[y+1][x] != GROUND:
-                walls += 1
-            if dungeon[y+1][x+1] != GROUND:
-                walls += 1
-
-            if walls > 5:
-                dungeon[y][x] = EARTH
-            elif walls < 4:
-                dungeon[y][x] = GROUND
-                       
-    return dungeon
-
-def clean_dungeon(dungeon, orig, dest):
-    for y in range(1,len(dungeon)-1):
-        for x in range(1,len(dungeon[y])-1):
-            if dungeon[y][x] == orig:
-                 dungeon[y][x] = dest
-    return dungeon
-
-def flood_dungeon(dungeon):
-    for y in range(1,len(dungeon)-1):
-        for x in range(1,len(dungeon[y])-1):
-            if dungeon[y][x] == GROUND:
-                dungeon = flood_dungeon_tile(dungeon, (x,y))
-                dungeon = clean_dungeon(dungeon, WET_EARTH, DRY_EARTH)
-
-    return dungeon
-
-def flood_dungeon_tile(dungeon, pos):
-    x,y = pos
-    tile = dungeon[y][x]
-    if tile == GROUND:
-        dungeon[y][x] = FLOOR
-        dungeon = flood_dungeon_tile(dungeon, (x,y-1))
-        dungeon = flood_dungeon_tile(dungeon, (x-1,y))
-        dungeon = flood_dungeon_tile(dungeon, (x+1,y))
-        dungeon = flood_dungeon_tile(dungeon, (x,y+1))
-    elif tile == EARTH:
-        dungeon[y][x] = WET_EARTH
-    elif tile == DRY_EARTH:
-        dungeon[y][x] = UNKNOWN
-
-    return dungeon
-    
-def print_dungeon(dungeon):
-    for line in dungeon:
-        linestr = []
-        for char in line:
-            if char == GROUND:
-                linestr.append(FLOOR)
-            else:
-                linestr.append(WALL)
-        print("".join(linestr))
-    print("")
-    
-def dump_dungeon(dungeon):
-    for line in dungeon:
-        print("".join(line))
-    print("")
-    
-def gen1():
-    dungeon = gen_dungeon(17,60,.40)
-    for i in range(0,3):
-        dungeon = evolve_dungeon(dungeon)
-    dungeon = clean_dungeon(dungeon, UNKNOWN, GROUND)
-    dungeon = flood_dungeon(dungeon)
-    dungeon = clean_dungeon(dungeon, DRY_EARTH, WALL)
-    dungeon = clean_dungeon(dungeon, UNKNOWN, FLOOR)
-    
-    dump_dungeon(dungeon)
-
-def gen2():
-    dungeon = gen_dungeon(17,30,.40)
-    print_dungeon(dungeon)
-    dungeon = evolve_dungeon(dungeon)
-    dump_dungeon(dungeon)
-    dungeon = clean_dungeon(dungeon, UNKNOWN, GROUND)
-    dungeon = flood_dungeon(dungeon)
-    dungeon = clean_dungeon(dungeon, DRY_EARTH, WALL)
-    dungeon = clean_dungeon(dungeon, UNKNOWN, FLOOR)
-    dump_dungeon(dungeon)
+    def __init__(self,prob):
+        self.prob = prob
         
+    def _gen_square(self):
+        if random() >= self.prob:
+            return Tile.EARTH
+        else:
+            return Tile.FLOOR
+    
+    def transform(self, map):
+        height = len(map)
+        width = len(map[0])
+
+        # first and last lines
+        for x in range(0,width):
+            map[0][x] = Tile.WALL
+            map[-1][x] = Tile.WALL
+
+        # other lines
+        for line in map[1:-1]:
+            line[0] = Tile.WALL
+            line[-1] = Tile.WALL
+            for x in range(1, width-1):
+                line[x] = self._gen_square()
+
+        return map
+
+class SmoothMap(MapTransform):
+
+    def transform(self,map):
+        for y in range(1,len(map)-1):
+            for x in range(1,len(map[y])-1):
+                       
+                walls = 0
+                if map[y-1][x-1] != Tile.FLOOR:
+                    walls += 1
+                if map[y-1][x] != Tile.FLOOR:
+                    walls += 1
+                if map[y-1][x+1] != Tile.FLOOR:
+                    walls += 1
+
+                if map[y][x-1] != Tile.FLOOR:
+                    walls += 1
+                if map[y][x+1] != Tile.FLOOR:
+                    walls += 1
+
+                if map[y+1][x-1] != Tile.FLOOR:
+                    walls += 1
+                if map[y+1][x] != Tile.FLOOR:
+                    walls += 1
+                if map[y+1][x+1] != Tile.FLOOR:
+                    walls += 1
+
+                if walls > 5:
+                    map[y][x] = Tile.EARTH
+                elif walls < 4:
+                    map[y][x] = Tile.FLOOR
+                       
+        return map
+
+class HardenWallsMap(MapTransform):
+
+    def transform(self, map):
+        flooded = set()
+        for y in range(1,len(map)-1):
+            for x in range(1,len(map[y])-1):
+                if map[y][x] == Tile.FLOOR:
+                    map = self._flood_tile(map, (x,y), flooded)
+        return map
+
+    def _flood_tile(self, map, pos, flooded):
+        if pos in flooded:
+            return map
+
+        x,y = pos
+        tile = map[y][x]
+        if tile == Tile.FLOOR:
+            flooded.add(pos)
+            map = self._flood_tile(map, (x,y-1), flooded)
+            map = self._flood_tile(map, (x-1,y), flooded)
+            map = self._flood_tile(map, (x+1,y), flooded)
+            map = self._flood_tile(map, (x,y+1), flooded)
+        elif tile == Tile.EARTH:
+            map[y][x] = Tile.WALL
+        return map
+
+class MapRender(object):
+
+    def add_map(self, map):
+        raise NotImplemented
+    def render(self, map):
+        raise NotImplemented
+
+class TextRenderer(MapRender):
+        
+    def render(self, map):
+        buffer = []
+        for line in map:
+            for pos in line:
+                if pos == Tile.WALL:
+                    buffer.append('#')
+                elif pos == Tile.FLOOR:
+                    buffer.append(' ')
+                elif pos == Tile.EARTH:
+                    buffer.append('+')
+                elif pos == Tile.WET:
+                    buffer.append('~')
+                elif pos == Tile.SAND:
+                    buffer.append('.')
+                else:
+                    buffer.append('?')
+            buffer.append(os.linesep)
+        return "".join(buffer)
+    
+class Animator(object):
+
+    def __init__(self, renderer):
+        self.renderer = renderer
+        self.maps = []
+
+    def _clear_console(self):
+        if os.name == 'posix':
+            os.system('clear')
+        else:
+            os.system('CLS')
+            
+    def add_map(self, map, deepcopy=False):
+        if deepcopy:
+            self.maps.append(copy.deepcopy(map))
+        else:
+            self.maps.append(map)
+
+    def animate(self, delay=1):
+        for map in self.maps:
+            # Clear the console
+            self._clear_console()
+
+            outstr = self.renderer.render(map)
+            # Write the current frame on stdout and sleep
+            
+            sys.stdout.write(outstr)
+            sys.stdout.flush()
+            time.sleep(delay)    
+
+def main():
+
+    WIDTH = 40
+    HEIGHT= 25
+    PROB = 0.4
+    
+    map = [[None for _ in range(0, WIDTH)] for _ in range(0, HEIGHT)]
+    
+    creator = RandomizeMap(PROB)
+    smooth = SmoothMap()
+    flood = HardenWallsMap()
+    renderer = TextRenderer()
+    animator = Animator(renderer)
+
+    map = creator.transform(map)
+    animator.add_map(map,True)
+    
+    map = smooth.transform(map)
+    animator.add_map(map,True)
+
+    # map = smooth.transform(map)
+    # animator.add_map(map,True)
+
+    map = flood.transform(map)
+    animator.add_map(map,True)
+    
+    animator.animate()    
+    
 if __name__ == '__main__':
-    gen1()
+    main()
