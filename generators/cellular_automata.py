719314952fdc9d0ae7b9631b958afddeb9041874
joaohenriques <joaopedrohenriques@gmail.com>
2015-04-04 15:41:52 +0100

    added Position class. changed flood algorithms to non-recursive

--- generators/cellular_automata.py
+++ generators/cellular_automata.py
@@ -7,7 +7,7 @@ from random import random, choice
 from enum import Enum
 from abc import ABCMeta, abstractmethod
 from math import sqrt
-
+from collections import namedtuple
 
 Tile = Enum('Tile', 'UNKNOWN EARTH WALL FLOOR')
 
@@ -27,17 +27,84 @@ class Cave(object):
 
     def get(self, pos):
         x, y = pos
-        return self._map[y][x]
+        try:
+            return self._map[y][x]
+        except IndexError:
+            # TODO log
+            return Tile.UNKNOWN
 
     def set(self, pos, val):
         x, y = pos
-        self._map[y][x] = val
+        try:
+            self._map[y][x] = val
+        except IndexError:
+            # TODO log
+            pass
 
-    def keys(self):
-        return ((x, y) for y in range(0, self.height) for x in range(0, self.width))
+    def keys(self, start_pos=None, end_pos=None):
+        if not start_pos:
+            start_pos = (0, 0)
+        if not end_pos:
+            end_pos = (self.width, self.height)
+        return (Position(x, y) for y in range(start_pos[1], end_pos[1]) for x in range(start_pos[0], end_pos[0]))
 
-    def items(self):
-        return (((x, y), self._map[y][x]) for y in range(0, self.height) for x in range(0, self.width))
+    def values(self, start_pos=None, end_pos=None):
+        if not start_pos:
+            start_pos = (0, 0)
+        if not end_pos:
+            end_pos = (self.width, self.height)
+        return (self._map[y][x] for y in range(start_pos[1], end_pos[1]) for x in range(start_pos[0], end_pos[0]))
+
+    def items(self, start_pos=None, end_pos=None):
+        if not start_pos:
+            start_pos = (0,0)
+        if not end_pos:
+            end_pos = (self.width, self.height)
+        return ((Position(x, y), self._map[y][x]) for y in range(start_pos[1], end_pos[1]) for x in range(start_pos[0], end_pos[0]))
+
+
+class Position(namedtuple('Point', ['x', 'y'])):
+
+    def __init__(self, x, y, **kwds):
+        super(tuple, self).__init__(x, y, **kwds)
+
+    @property
+    def n(self):
+        return Position(self.x, self.y-1)
+
+    @property
+    def ne(self):
+        return Position(self.x+1, self.y-1)
+
+    @property
+    def e(self):
+        return Position(self.x+1, self.y)
+
+    @property
+    def se(self):
+        return Position(self.x+1, self.y+1)
+
+    @property
+    def s(self):
+        return Position(self.x, self.y+1)
+
+    @property
+    def sw(self):
+        return Position(self.x-1, self.y+1)
+
+    @property
+    def w(self):
+        return Position(self.x-1, self.y)
+
+    @property
+    def nw(self):
+        return Position(self.x-1, self.y-1)
+
+    def __repr__(self):
+        return "Position(x={}, y={})".format(self.x, self.y)
+
+    def __str__(self):
+        return "({}, {})".format(self.x, self.y)
 
 
 class CaveGenerationCommand(object):
@@ -64,53 +131,40 @@ class RandomizeCave(CaveGenerationCommand):
             return Tile.FLOOR
     
     def execute(self, cave):
-        width = len(cave[0])
 
-        # first and last lines
-        for x in range(0, width):
-            cave[0][x] = Tile.WALL
-            cave[-1][x] = Tile.WALL
-
-        # other lines
-        for line in cave[1:-1]:
-            line[0] = Tile.WALL
-            line[-1] = Tile.WALL
-            for x in range(1, width-1):
-                line[x] = self._gen_square()
+        for pos in cave.keys():
+            cave.set(pos,self._gen_square())
 
         return cave
-
 
 class SmoothCave(CaveGenerationCommand):
 
     def execute(self, cave):
-        for y in range(1, len(cave)-1):
-            for x in range(1, len(cave[y])-1):
-                       
-                walls = 0
-                if cave[y-1][x-1] != Tile.FLOOR:
-                    walls += 1
-                if cave[y-1][x] != Tile.FLOOR:
-                    walls += 1
-                if cave[y-1][x+1] != Tile.FLOOR:
-                    walls += 1
+        for pos in cave.keys():
+            walls = 0
+            if cave.get(pos.nw) != Tile.FLOOR:
+                walls += 1
+            if cave.get(pos.n) != Tile.FLOOR:
+                walls += 1
+            if cave.get(pos.ne) != Tile.FLOOR:
+                walls += 1
 
-                if cave[y][x-1] != Tile.FLOOR:
-                    walls += 1
-                if cave[y][x+1] != Tile.FLOOR:
-                    walls += 1
+            if cave.get(pos.w) != Tile.FLOOR:
+                walls += 1
+            if cave.get(pos.e) != Tile.FLOOR:
+                walls += 1
 
-                if cave[y+1][x-1] != Tile.FLOOR:
-                    walls += 1
-                if cave[y+1][x] != Tile.FLOOR:
-                    walls += 1
-                if cave[y+1][x+1] != Tile.FLOOR:
-                    walls += 1
+            if cave.get(pos.sw) != Tile.FLOOR:
+                walls += 1
+            if cave.get(pos.s) != Tile.FLOOR:
+                walls += 1
+            if cave.get(pos.se) != Tile.FLOOR:
+                walls += 1
 
-                if walls > 5:
-                    cave[y][x] = Tile.EARTH
-                elif walls < 4:
-                    cave[y][x] = Tile.FLOOR
+            if walls > 5:
+                cave.set(pos, Tile.EARTH)
+            elif walls < 4:
+                cave.set(pos, Tile.FLOOR)
                        
         return cave
 
@@ -119,57 +173,61 @@ class HardenWallsCave(CaveGenerationCommand):
 
     def execute(self, cave):
         flooded = set()
-        for y in range(1, len(cave)-1):
-            for x in range(1, len(cave[y])-1):
-                if cave[y][x] == Tile.FLOOR:
-                    cave = self._flood_tile(cave, (x, y), flooded)
+        for pos in cave.keys():
+            cave = self._flood_tile(cave, pos, flooded)
         return cave
 
-    def _flood_tile(self, cave, pos, flooded):
-        if pos in flooded:
-            return cave
+    @staticmethod
+    def _flood_tile(cave, pos, flooded):
+        stack = [pos]
 
-        x,y = pos
-        tile = cave[y][x]
-        if tile == Tile.FLOOR:
-            flooded.add(pos)
-            cave = self._flood_tile(cave, (x,y-1), flooded)
-            cave = self._flood_tile(cave, (x-1,y), flooded)
-            cave = self._flood_tile(cave, (x+1,y), flooded)
-            cave = self._flood_tile(cave, (x,y+1), flooded)
-        elif tile == Tile.EARTH:
-            cave[y][x] = Tile.WALL
+        while len(stack) > 0:
+            pos = stack.pop()
+
+            if pos in flooded:
+                continue
+
+            tile = cave.get(pos)
+            if tile == Tile.FLOOR:
+                flooded.add(pos)
+                stack.append(pos.n)
+                stack.append(pos.e)
+                stack.append(pos.s)
+                stack.append(pos.w)
+
+            elif tile == Tile.EARTH:
+                cave.set(pos, Tile.WALL)
+
         return cave
 
 
 class CloseSingleRooms(CaveGenerationCommand):
 
     def execute(self, cave):
-        for y in range(1, len(cave)-1):
-            for x in range(1, len(cave[y])-1):
-                if cave[y][x] == Tile.FLOOR and \
-                   cave[y+1][x] == Tile.EARTH and \
-                   cave[y][x+1] == Tile.EARTH:
+        for pos in cave.keys():
+            if cave.get(pos) == Tile.FLOOR and \
+               cave.get(pos.s) != Tile.FLOOR and \
+               cave.get(pos.e) != Tile.FLOOR:
 
-                    if cave[y+1][x+1] == Tile.FLOOR:
-                        if random() > 0.5:
-                            cave[y+1][x] = Tile.FLOOR
-                        else:
-                            cave[y][x+1] = Tile.FLOOR
-
-                    elif cave[y-1][x] == Tile.EARTH and \
-                       cave[y][x-1] == Tile.EARTH:
-
-                        cave[y][x] = Tile.EARTH
-
-                elif cave[y][x] == Tile.EARTH and \
-                   cave[y+1][x] == Tile.FLOOR and \
-                   cave[y][x+1] == Tile.FLOOR and \
-                   cave[y+1][x+1] == Tile.EARTH:
+                if cave.get(pos.sw) == Tile.FLOOR:
                     if random() > 0.5:
-                        cave[y][x] = Tile.FLOOR
+                        cave.set(pos.s, Tile.FLOOR)
                     else:
-                        cave[y+1][x+1] = Tile.FLOOR
+                        cave.set(pos.e, Tile.FLOOR)
+
+                elif cave.get(pos.n) != Tile.FLOOR and \
+                        cave.get(pos.w) != Tile.FLOOR:
+
+                    cave.set(pos, Tile.EARTH)
+
+            elif cave.get(pos) != Tile.FLOOR and \
+                 cave.get(pos.s) == Tile.FLOOR and \
+                 cave.get(pos.e) == Tile.FLOOR and \
+                 cave.get(pos.se) != Tile.FLOOR:
+                if random() > 0.5:
+                    cave.set(pos, Tile.FLOOR)
+                else:
+                    cave.set(pos.sw, Tile.FLOOR)
 
         return cave
 
@@ -226,8 +284,8 @@ class LinkRooms(CaveGenerationCommand):
             elif yt < ys:
                 yd -= 1
 
-        cave[yd][xd] = Tile.FLOOR
-        return self.dig(cave,(xd,yd),(xt,yt))
+        cave.set((xd,yd), Tile.FLOOR)
+        return self.dig(cave, (xd, yd), (xt, yt))
 
     @staticmethod
     def _calculate_distance(room_a, room_b):
@@ -239,42 +297,47 @@ class LinkRooms(CaveGenerationCommand):
                 y = abs(ya-yb)
                 distance = sqrt(pow(x,2) + pow(y,2))
                 if distance == min_distance:
-                    closest.append(((xa,ya),(xb,yb)))
+                    closest.append(((xa, ya), (xb, yb)))
                 elif distance < min_distance:
                     min_distance = distance
-                    closest = [((xa,ya),(xb,yb))]
+                    closest = [((xa, ya), (xb, yb))]
 
-        return (min_distance, closest)
+        return min_distance, closest
     
     def _find_rooms(self, cave):
         rooms = []
-        for y in range(1,len(cave)-1):
-            for x in range(1,len(cave[y])-1):
-                if cave[y][x] == Tile.FLOOR:
-                    pos = (x,y)
-                    in_room = False
-                    for room in rooms:
-                        if pos in room:
-                            in_room = True
-                            break
-                    if not in_room:
-                        rooms.append(self._flood_tile(cave, pos))
+        for pos in cave.keys():
+            if cave.get(pos) == Tile.FLOOR:
+                in_room = False
+                for room in rooms:
+                    if pos in room:
+                        in_room = True
+                        break
+                if not in_room:
+                    rooms.append(self._it_flood_room(cave, pos))
 
         return rooms
-    
-    def _flood_tile(self, cave, pos, flooded=None):
-        if not flooded:
-            flooded = set()
-        x,y = pos
-        if pos not in flooded and cave[y][x] == Tile.FLOOR:
-            flooded.add(pos)
-            flooded = self._flood_tile(cave, (x,y-1), flooded)
-            flooded = self._flood_tile(cave, (x-1,y), flooded)
-            flooded = self._flood_tile(cave, (x+1,y), flooded)
-            flooded = self._flood_tile(cave, (x,y+1), flooded)
 
-        return flooded
+    @staticmethod
+    def _it_flood_room(cave, pos):
+        room = set()
+        stack = [pos]
 
+        while len(stack) > 0:
+            pos = stack.pop()
+
+            if pos in room:
+                continue
+
+            tile = cave.get(pos)
+            if tile == Tile.FLOOR:
+                room.add(pos)
+                stack.append(pos.n)
+                stack.append(pos.e)
+                stack.append(pos.s)
+                stack.append(pos.w)
+
+        return room
 
 class MapRender(object):
     __metaclass__ = ABCMeta
@@ -288,21 +351,33 @@ class TextRenderer(MapRender):
         
     def render(self, cave):
         str_buffer = []
-        for line in cave:
-            for pos in line:
-                if pos == Tile.WALL:
-                    str_buffer.append('#')
-                elif pos == Tile.FLOOR:
-                    str_buffer.append(' ')
-                elif pos == Tile.EARTH:
-                    str_buffer.append('+')
-                elif pos == Tile.DIG:
-                    str_buffer.append('.')
-                #elif pos == Tile.SAND:
-                #    buffer.append('.')
-                else:
-                    str_buffer.append(str(pos))
-            str_buffer.append(os.linesep)
+        width = cave.width
+        str_buffer.append("#" * (width+2))
+        str_buffer.append("\n")
+        str_buffer.append("#")
+        col = 0
+        for val in cave.values():
+            col += 1
+            if val == Tile.WALL:
+                str_buffer.append('#')
+            elif val == Tile.FLOOR:
+                str_buffer.append(' ')
+            elif val == Tile.EARTH:
+                str_buffer.append('+')
+            elif val == Tile.DIG:
+                str_buffer.append('.')
+            #elif val == Tile.SAND:
+            #    buffer.append('.')
+            else:
+                str_buffer.append(str(val))
+
+            if col % width == 0:
+                col = 0
+                str_buffer.append("#")
+                str_buffer.append("\n")
+                str_buffer.append("#")
+        str_buffer.append("#" * (width+1))
+
         return "".join(str_buffer)
 
 
@@ -339,20 +414,19 @@ class Animator(object):
 
 def main():
 
-    WIDTH = 80
+    WIDTH = 77
     HEIGHT= 20
     PROB = 0.4
-    
-    map = [[None for _ in range(0, WIDTH)] for _ in range(0, HEIGHT)]
 
+    cave = Cave(HEIGHT, WIDTH)
     creator = RandomizeCave(PROB)
     smooth = SmoothCave()
     closerooms = CloseSingleRooms()
     flood = HardenWallsCave()
-    linkroom = LinkRooms(0.20)
+    linkroom = LinkRooms(.2)
     
     renderer = TextRenderer()
-    animator = Animator(renderer, map)
+    animator = Animator(renderer, cave)
 
     animator.add_command(creator)
     animator.add_command(smooth)
@@ -362,9 +436,10 @@ def main():
     animator.add_command(linkroom)
     animator.add_command(linkroom)
     animator.add_command(linkroom)
+    animator.add_command(linkroom)
     animator.add_command(flood)
     
-    animator.animate(delay=0.2)
+    animator.animate(delay=0.1)
     
 if __name__ == '__main__':
     main()
